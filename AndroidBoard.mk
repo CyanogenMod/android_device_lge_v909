0f8d4f6d6d18c2798efa26fc6c2b0047b919bcd3
Aaron Echols <atechols@gmail.com>
2012-08-08 16:32:13 -0700

    v909: jelly bean fixes for boot
    * init fixes
    * added more efficient jvm heap options
    * fixed emmc corruption
    * add correct density settings -> system.prop
    * added temp support for using a local vendor tree, until I can fix
      the current jellybean vendor tree
    * fixed audio -- need to get other audio devices fixed
    * fixed some white spaces
    
    Change-Id: I1bcf22fec397ddec35fb1f0337e626fa4891dfe5

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,5 +4,8 @@ include $(CLEAR_VARS)
 
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
+subdir_makefiles:= \
+    $(LOCAL_PATH)/audio/Android.mk
+
 # include the non-open-source counterpart to this file
 -include vendor/lge/v909/AndroidBoardVendor.mk
--- v909.mk
+++ v909.mk
@@ -3,7 +3,10 @@ $(call inherit-product, build/target/product/full_base.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/lge/v909/v909-vendor.mk)
+# Temp make file for pulling the local vendor tree -- Aaron Echols
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+#$(call inherit-product-if-exists, vendor/lge/v909/v909-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/lge/v909/overlay
 
@@ -30,51 +33,31 @@ PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/gps.conf:system/etc/gps.conf
 
 PRODUCT_COPY_FILES += \
-     $(LOCAL_PATH)/tiny_hw.xml:/system/etc/sound/v909.xml
+     $(LOCAL_PATH)/tiny_hw.xml:/system/etc/sound/tiny_hw.xml
 
 PRODUCT_PROPERTY_OVERRIDES := \
     wifi.interface=eth0 \
     wifi.supplicant_scan_interval=15
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
-    frameworks/native/data/etc/com.nvidia.display.xml:system/etc/permissions/com.nvidia.display.xml \
-    frameworks/native/data/etc/com.tmus.gslate.apps.xml:system/etc/permissions/com.tmus.gslate.apps.xml \
-    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
-
-PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml
+
+PRODUCT_PACKAGES += \
+    HoloSpiralWallpaper \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    Camera
 
 ## LGE stuffs
 PRODUCT_PACKAGES += \
     LGEServices \
-    audio.primary.startablet \
-    libaudioutils \
     bridgeutil \
     libbridges \
     libbridge \
     libbridge_jni \
-    lights.startablet \
+    libaudioutils \
+    libtinyalsa \
+    audio.primary.startablet \
     secureclockd \
     libsecureclock \
     screencap \
@@ -82,13 +65,11 @@ PRODUCT_PACKAGES += \
     com.android.future.usb.accessory \
     e2fsck \
     make_ext4fs \
+    vold \
     setup_fs
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=160
 
 PRODUCT_CHARACTERISTICS := tablet,nosdcard
 
